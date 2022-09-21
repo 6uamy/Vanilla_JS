5097e71002e168a633a48889c19e0cc489937cda
Hwang Seong Taek <hst007@naver.com>
2022-09-21 16:21:28 +0900

    eventListener

--- app.js
+++ app.js
@@ -1,3 +1,38 @@
-const title = document.getElementById('title');
+const h1 = document.querySelector('div.hello:first-child h1');
 
-title.innerText = 'got you';
+function onclicking(){
+    h1.style.color = 'blue';
+}
+
+function onhover(){
+    h1.innerText = 'Mouse is here';
+}
+
+function onleave(){
+    h1.innerText = 'Mouse is gone';
+}
+
+function handleWindowResize(){
+    document.body.style.backgroundColor = 'tomato';
+}
+
+function handleWindowcopy(){
+    alert('Copier!');
+}
+
+function handleoff(){
+    alert('No wifi!');
+}
+
+function handleon(){
+    alert('good');
+}
+
+h1.addEventListener('click', onclicking);
+h1.addEventListener('mouseenter', onhover);
+h1.addEventListener('mouseleave', onleave);
+
+window.addEventListener('resize', handleWindowResize);
+window.addEventListener('copy', handleWindowcopy);
+window.addEventListener('offline', handleoff);
+window.addEventListener('online', handleon);
